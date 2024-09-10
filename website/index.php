9ab4ce2593b57c9941f5b8e9ad96ae6dd71896d6
Gabriel <86844990+gabydomingo@users.noreply.github.com>
2024-09-09 22:15:50 -0300

    Add files via upload

---
+++ website/index.php
@@ -0,0 +1,23 @@
+<?php include "header.php" ?>
+
+
+<section class="cuerpo">
+    <div class="content">
+        <div>
+        <h1 class="tituloJapones">
+            もラーメン
+            <hr class="reglon">
+        </h1>
+        <p class="textoCuerpo">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit temporibus odit quidem qui quaerat corporis dicta cum veniam veritatis voluptatibus ab deleniti laudantium nisi et dignissimos ea nobis error, earum quos minus aut perferendis quis placeat eos. Atque dolorum distinctio expedita impedit, fugiat animi est ipsum dolorem quia doloremque porro, molestias m.
+        </p>
+        </div>
+    </div>
+    <div class="imgRamen">
+    <img src="img\logo\bowlRamen.png" alt="bowl de ramen">
+    </div>
+</section>
+
+
+
+<?php include "footer.php" ?>
