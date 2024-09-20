a3be5084345d408edaa95cc9f016a07b4564afd5
Gabriel <86844990+gabydomingo@users.noreply.github.com>
2024-09-19 21:50:36 -0300

    Delete website directory

--- website/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $conexion = mysqli_connect("localhost","root","","kumo_db");
-
-//     if($conexion){
-//         echo "Conectado a la base de datos";
-//     }else{
-//         echo "Error al conectar a la base de datos";
-//     }
-?>
